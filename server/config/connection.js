19c338da2d85d5ae47476393bc699b78ad6c7ff0
HelenELee <HelenELee3@outlook.com>
2023-04-22 20:59:07 +0800

    initial commit

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+//127.0.0.1:27017
+//localhost
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
