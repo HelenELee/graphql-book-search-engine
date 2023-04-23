deb885063616239f3e004a2b3dd820a3d885cf1c
HelenELee <HelenELee3@outlook.com>
2023-04-23 17:10:26 +0800

    useCreateIndex and useFindAndModify

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 //127.0.0.1:27017
 //localhost
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  //useCreateIndex: true,
+ // useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
