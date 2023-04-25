bc24c98af20fb353c9a087939ae76084046983f9
HelenELee <HelenELee3@outlook.com>
2023-04-25 12:52:02 +0800

    updated LoginForm to use graphQL

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,12 +3,17 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { loginUser } from '../utils/API';
+
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -24,7 +29,18 @@ const LoginForm = () => {
       event.preventDefault();
       event.stopPropagation();
     }
+    try {
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
 
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    
+    /*
     try {
       const response = await loginUser(userFormData);
 
@@ -39,6 +55,7 @@ const LoginForm = () => {
       console.error(err);
       setShowAlert(true);
     }
+    */
 
     setUserFormData({
       username: '',
